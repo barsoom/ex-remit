e8449c19e118c45ae0a4383d277dc5ec473c932a
Henrik Nyh <henrik@nyh.se>
2020-05-15 11:56:27 +0100

    Polish

--- lib/remit_web/live/page_live.ex
+++ lib/remit_web/live/page_live.ex
@@ -36,26 +36,6 @@ defmodule RemitWeb.PageLive do
     {:noreply, socket |> assign(results: %{}, commits: commits, query: "")}
   end
 
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, commits: [], results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, commits: [], query: query)}
-    end
-  end
-
   defp get_commits do
     import Ecto.Query
 
@@ -65,17 +45,5 @@ defmodule RemitWeb.PageLive do
         preload: :author
 
     Repo.all(query)
-  end
-
-  defp search(query) do
-    if not RemitWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
