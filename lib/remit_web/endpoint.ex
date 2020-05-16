12d2d027c11021c987240a6bef467f76e16ca047
Henrik Nyh <henrik@nyh.se>
2020-05-16 13:33:18 +0100

    Adapt settings for Heroku
    As outlined in https://hexdocs.pm/phoenix/heroku.html.

--- lib/remit_web/endpoint.ex
+++ lib/remit_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule RemitWeb.Endpoint do
   ]
 
   socket "/socket", RemitWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
