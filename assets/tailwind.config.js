d935825c829d4f6112001ef17aba2c1de9c2402b
Henrik Nyh <henrik@nyh.se>
2020-05-16 21:45:58 +0100

    Add Tailwind CSS
    Per these instructions: https://pragmaticstudio.com/tutorials/adding-tailwind-css-to-phoenix
    
    Sets up Purge CSS which aims to reduce the size of the prod CSS by removing unused selectors. If we ever see prod styling issues, that could be the culprit.

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
