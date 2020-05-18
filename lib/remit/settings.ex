cd57c61a1028a83e9adc0a3c2133be7c40459391
Henrik Nyh <henrik@nyh.se>
2020-05-18 23:55:29 +0100

    Delete stale Settings
    Compared to the number of Commits or Comments, it's unlikely we'd get very many Settings. But it just feels messy to keep stale ones.
    
    Could delete them in a recurring background process instead of inline, but this seems plenty fast enough.
    
    100 days should be enough, since we run this code on every `mount`, so every time you revisit the app or switch tabs in it (though I may change the app so every LiveView is mounted on start and not re-mounted on navigation).

--- lib/remit/settings.ex
+++ lib/remit/settings.ex
@@ -1,7 +1,10 @@
 defmodule Remit.Settings do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
   alias Remit.{Repo,Settings}
+
+  @stale_after_days 100
 
   schema "settings" do
     field :email, :string
@@ -20,6 +23,21 @@ defmodule Remit.Settings do
   end
 
   def for_session(%{"session_id" => sid}) do
-    Repo.get_by(Settings, session_id: sid) || %Settings{session_id: sid}
+    settings = Repo.get_by(Settings, session_id: sid)
+
+    if settings do
+      now = DateTime.utc_now()
+
+      # Update `read_at` so we can track stale Settings.
+      settings = settings |> Ecto.Changeset.change(read_at: now) |> Repo.update!
+
+      # Delete stale Settings so DB doesn't keep them forever. It's cheap enough to do on every call.
+      stale_before = now |> DateTime.add(-60 * 60 * 24 * @stale_after_days, :second)
+      Repo.delete_all(from s in Settings, where: s.read_at < ^stale_before)
+
+      settings
+    else
+      %Settings{session_id: sid}
+    end
   end
 end
