86fb3ef4f4d016f3665d552c41c0acaba104e661
Henrik Nyh <henrik@nyh.se>
2020-05-18 22:08:35 +0100

    Basic spec
    Mostly as an experiment to see how tests work with LiveView.

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :remit, Remit.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "henrik",
+  password: "",
   database: "remit_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
--- test/remit_web/live/commits_live_test.exs
+++ test/remit_web/live/commits_live_test.exs
@@ -1,11 +1,23 @@
 defmodule RemitWeb.CommitsLiveTest do
   use RemitWeb.ConnCase
-
   import Phoenix.LiveViewTest
+  alias Remit.{Repo,Settings}
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, commits_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+  test "renders name from per-session settings", %{conn: conn} do
+    conn = get(conn, "/")
+    session_id = conn |> get_session(:session_id)
+
+    # We haven't yet set our name.
+    assert html_response(conn, 200) =~ "Hello, stranger"
+
+    Repo.insert! %Settings{
+      session_id: session_id,
+      email: "user@example.com",
+      name: "Banani",
+    }
+
+    {:ok, live_view, disconnected_html} = live(conn, "/")
+    assert disconnected_html =~ "Hello, Banani"
+    assert render(live_view) =~ "Hello, Banani"
   end
 end
