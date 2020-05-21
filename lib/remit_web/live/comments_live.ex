fba02a133e43a73757d0520b6643ece83aeb70f9
Henrik Nyh <henrik@nyh.se>
2020-05-21 11:14:36 +0100

    Stop ticking time on CommentsLive
    It makes it more difficult to see what's going on in the WebSocket, with constant messages.

--- lib/remit_web/live/comments_live.ex
+++ lib/remit_web/live/comments_live.ex
@@ -3,20 +3,6 @@ defmodule RemitWeb.CommentsLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    IO.inspect {:mount, :comments, connected?(socket)}
-    if connected?(socket), do: :timer.send_interval(100, self(), :tick)
-
-    socket = assign(socket, %{
-      time: now(),
-    })
-
     {:ok, socket}
   end
-
-  @impl true
-  def handle_info(:tick, socket) do
-    {:noreply, assign(socket, :time, now())}
-  end
-
-  defp now, do: DateTime.utc_now()
 end
