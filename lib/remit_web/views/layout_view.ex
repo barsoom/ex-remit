524508fc5181a40a90ed2f09e663261ab6882cfc
Henrik Nyh <henrik@nyh.se>
2020-05-21 18:11:52 +0100

    Styling

--- lib/remit_web/views/layout_view.ex
+++ lib/remit_web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule RemitWeb.LayoutView do
   use RemitWeb, :view
+
+  defp tab(action, text, icon, socket, live_action) do
+    live_patch(to: Routes.tabs_path(socket, action), class: "tabs__tab #{if live_action == action, do: "tabs__tab--#{action} tabs__tab--current"}") do
+      ~E"""
+      <i class="fas <%= icon %> mr-1"></i>
+      <%= text %>
+      """
+    end
+  end
 end
