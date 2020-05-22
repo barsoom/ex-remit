152d89b3ce1d0247d6455962c0ab9add0c89b76e
Henrik Nyh <henrik@nyh.se>
2020-05-22 14:53:03 +0100

    Extract 'gravatar' helper

--- lib/remit_web/live/commit_component.ex
+++ lib/remit_web/live/commit_component.ex
@@ -2,4 +2,12 @@ defmodule RemitWeb.CommitComponent do
   use Phoenix.LiveComponent
 
   alias Remit.Commit
+
+  defp gravatar(email, class) do
+    assigns = %{}
+
+    ~L"""
+    <img src="<%= Gravatar.url(email) %>" alt="" title="<%= email %>" class="<%= class %>" />
+    """
+  end
 end
