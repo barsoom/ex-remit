c65a4de2e31423ca3b436a244ca1ca97026a5b35
Henrik Nyh <henrik@nyh.se>
2020-05-23 00:10:36 +0100

    Revert "Remove nprogress dependency"
    And revert "Oops, remove more of nprogress".
    
    This reverts commit a2764f8d28121e5a03784e217b9946907e08b9f5.
    This reverts commit dcc68d746af0625311ad447f119c81a02fbf5781.
    
    Probably a bad idea to have no indicator, since we can't control if
    latency is sometimes high. But we'll delay its onset in the next commit
    so we don't see it most of the time.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 import {Socket} from "phoenix"
+import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
