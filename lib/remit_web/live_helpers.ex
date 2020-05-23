f41db50255c7972d1cb67c866c060591c5a286d8
Henrik Nyh <henrik@nyh.se>
2020-05-24 00:50:26 +0100

    Debug

--- lib/remit_web/live_helpers.ex
+++ lib/remit_web/live_helpers.ex
@@ -7,6 +7,7 @@ defmodule RemitWeb.LiveHelpers do
   @expected_auth_key Application.get_env(:remit, :auth_key)
 
   def check_auth_key(session) do
+    IO.inspect [debug_session: session, expected_auth_key_length: String.length(@expected_auth_key)]
     if session["auth_key"] != @expected_auth_key, do: throw("Invalid auth_key in LiveView!")
   end
 end
