6b3a53d1eb9b04af674cbc2b5ff83579f2fb253b
Henrik Nyh <henrik@nyh.se>
2020-05-24 12:27:44 +0100

    Test cleanup

--- test/remit/commit_test.exs
+++ test/remit/commit_test.exs
@@ -2,9 +2,11 @@ defmodule Remit.CommitTest do
   use ExUnit.Case
   alias Remit.Commit
 
-  test "message_summary extracts the first line of the message" do
-    assert Commit.message_summary(%Commit{message: "My summary\nMore info"}) == "My summary"
-    assert Commit.message_summary(%Commit{message: "My summary\rMore info"}) == "My summary"
-    assert Commit.message_summary(%Commit{message: "My summary\r\nMore info"}) == "My summary"
+  describe "message_summary" do
+    test "extracts the first line of the message" do
+      assert Commit.message_summary(%Commit{message: "My summary\nMore info"}) == "My summary"
+      assert Commit.message_summary(%Commit{message: "My summary\rMore info"}) == "My summary"
+      assert Commit.message_summary(%Commit{message: "My summary\r\nMore info"}) == "My summary"
+    end
   end
 end
