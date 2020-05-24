275542282966c47c99b6d75f04692b1bae751821
Henrik Nyh <henrik@nyh.se>
2020-05-24 12:16:19 +0100

    Unbork spec
    We now require an auth_key, and we don't say 'stranger'.

--- test/remit_web/live/commits_live_test.exs
+++ test/remit_web/live/commits_live_test.exs
@@ -4,10 +4,11 @@ defmodule RemitWeb.CommitsLiveTest do
 
   test "says hello", %{conn: conn} do
     # We haven't yet set our name.
-    assert html_response(conn, 200) =~ "Hello, stranger"
+     conn = get(conn, "/?auth_key=test_auth_key")
+    assert html_response(conn, 200) =~ "Hello!"
 
     {:ok, live_view, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Hello, stranger"
-    assert render(live_view) =~ "Hello, stranger"
+    assert disconnected_html =~ "Hello!"
+    assert render(live_view) =~ "Hello!"
   end
 end
