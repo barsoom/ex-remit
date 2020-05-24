53daed0f22cdcaa997f59d89b20532f6e05e7da9
Henrik Nyh <henrik@nyh.se>
2020-05-24 12:08:41 +0100

    Fix warning

--- lib/remit_web/live_helpers.ex
+++ lib/remit_web/live_helpers.ex
@@ -2,8 +2,6 @@
 # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#module-security-considerations-of-the-liveview-model
 
 defmodule RemitWeb.LiveHelpers do
-  import Phoenix.LiveView
-
   @expected_auth_key Application.get_env(:remit, :auth_key)
 
   def check_auth_key(session) do
