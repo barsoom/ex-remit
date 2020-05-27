7377d59184ab85afe1a36dfbeb7ea118948ba1a6
Henrik Nyh <henrik@nyh.se>
2020-05-27 20:16:51 +0100

    Add 'mix ecto.seed' alias

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Remit.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
       "wh.commits": ["run priv/repo/fake_webhook_commits.exs"],
