c64023f8b75e784ca53b5b0ffddd041c78f79e48
Henrik Nyh <henrik@nyh.se>
2020-05-30 00:57:15 +0100

    Doc Tailwind config colours

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     "./js/**/*.js",
   ],
   theme: {
+
+    // This implements our own, more limited color palette within the broader default one. If we just want "some lightish yellow", we can just pick a default colour by feel, nevermind that we may use `-200` in one place and `-300` in another. If we do something where we care more about sticking to a limited palette, we use these.
+    //
+    // Longer term we may end up replacing all the default colours, but until then it's useful to have them to fall back on.
     textColor: theme => ({
       ...theme("colors"),
       "gray-mid": theme("colors.gray.600"),
@@ -21,6 +25,7 @@ module.exports = {
       ...theme("colors"),
       "green-light": theme("colors.green.100"),
     }),
+
     extend: {
     },
   },
