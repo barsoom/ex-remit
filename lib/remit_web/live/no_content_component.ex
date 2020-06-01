b0f5aae9dcd4d7558956ac44348a1e57790801bf
Henrik Nyh <henrik@nyh.se>
2020-06-01 22:49:25 +0100

    Show nicer empty state for Commits, too
    To match what we do for Comments.

---
+++ lib/remit_web/live/no_content_component.ex
@@ -0,0 +1,12 @@
+defmodule RemitWeb.NoContentComponent do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~L"""
+    <div class="mt-10 text-center">
+      <p><i class="fad fa-star-shooting text-6xl text-yellow-500"></i></p>
+      <p class="text-lg text-gray-mid pt-3">Nothing yet!</p>
+    </div>
+    """
+  end
+end
