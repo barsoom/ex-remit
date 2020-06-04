c0c36ee95f3a931cfd5bebecc678bddcfe34cd2d
Henrik Nyh <henrik@nyh.se>
2020-06-04 23:53:50 +0100

    Make 'Settings' tab smaller
    It's something you probably only use once.
    
    Have considered hiding it even more, not having it be a top-level tab. Maybe later.

--- lib/remit_web/views/layout_view.ex
+++ lib/remit_web/views/layout_view.ex
@@ -2,10 +2,10 @@ defmodule RemitWeb.LayoutView do
   use RemitWeb, :view
 
   defp tab(action, text, icon, socket, live_action) do
-    live_patch(to: Routes.tabs_path(socket, action), class: "tabs__tab #{if live_action == action, do: "tabs__tab--#{action} tabs__tab--current"}") do
+    live_patch(to: Routes.tabs_path(socket, action), class: "tabs__tab tabs__tab--#{action} #{if live_action == action, do: "tabs__tab--current"}") do
       ~E"""
-      <i class="fas <%= icon %> mr-1"></i>
-      <%= text %>
+      <i class="fas <%= icon %>"></i>
+      <span class="tabs__tab__text"><%= text %></span>
       """
     end
   end
