dcacc09b83b400e5af4169f77cce751dfe4631a4
Henrik Nyh <henrik@nyh.se>
2020-06-06 10:52:05 +0100

    Unbork spec after adding redirect from / to /commits

--- test/remit_web/live/commits_live_test.exs
+++ test/remit_web/live/commits_live_test.exs
@@ -4,10 +4,10 @@ defmodule RemitWeb.CommitsLiveTest do
 
   test "says hello", %{conn: conn} do
     # We haven't yet set our name.
-    conn = get(conn, "/?auth_key=test_auth_key")
+    conn = get(conn, "/commits?auth_key=test_auth_key")
     assert html_response(conn, 200) =~ "Nothing yet!"
 
-    {:ok, live_view, disconnected_html} = live(conn, "/")
+    {:ok, live_view, disconnected_html} = live(conn, "/commits")
     assert disconnected_html =~ "Nothing yet!"
     assert render(live_view) =~ "Nothing yet!"
   end
