e4aea14b8f171f1df195bb9097911257810918d5
Henrik Nyh <henrik@nyh.se>
2020-06-13 00:05:53 +0100

    Indicate when username is missing
    As seen e.g. in (our private) commit https://github.com/barsoom/live_bidding/commit/c74404360d1397bc5cc4b88dc7be7f4579ab1637 where the author was not associated with a username.
    
    I don't want to complicate Remit to handle this beyond showing it as an error.
    
    Other than looking bad, it doesn't cause a lot of issues. The commit can still be marked as reviewed, but Remit won't know to notify the authors about comments, and the author won't see the commit as being their own.

--- lib/remit_web/live/commit_component.ex
+++ lib/remit_web/live/commit_component.ex
@@ -2,6 +2,18 @@ defmodule RemitWeb.CommitComponent do
   use RemitWeb, :live_component
   alias Remit.{Commit, Utils}
 
+  defp usernames(%Commit{usernames: []}) do
+    content_tag(:span,
+      title: "Read about commit usernames under the \"Settings\" tab.",
+      class: "bg-gray-light rounded py-px px-1 text-gray-dark"
+    ) do
+      [
+        content_tag(:i, "", class: "fas fa-exclamation-circle text-red-600"),
+        " no username",
+      ]
+    end
+  end
+
   defp usernames(%Commit{usernames: names}) do
     names
     |> Enum.map(fn (name) ->
