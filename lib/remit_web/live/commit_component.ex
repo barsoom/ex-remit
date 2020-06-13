106c3f1ded9b08d2eab4487116630da721008f06
Henrik Nyh <henrik@nyh.se>
2020-06-13 11:43:59 +0100

    More tooltips, remove committer usernames
    Since the tooltips provide them.
    
    Commenter avatars don't yet show tooltips for some reason.

--- lib/remit_web/live/commit_component.ex
+++ lib/remit_web/live/commit_component.ex
@@ -1,34 +1,4 @@
 defmodule RemitWeb.CommitComponent do
   use RemitWeb, :live_component
   alias Remit.{Commit, Utils}
-
-  defp usernames(%Commit{usernames: []}) do
-    content_tag(:span,
-      tooltip_attributes("Read about usernames under the \"Settings\" tab.") ++ [
-        class: "bg-gray-light rounded py-px px-1 text-gray-dark",
-      ]
-    ) do
-      [
-        content_tag(:i, "", class: "fas fa-exclamation-circle text-red-600"),
-        " no username",
-      ]
-    end
-  end
-
-  defp usernames(%Commit{usernames: names}) do
-    names
-    |> Enum.map(fn (name) ->
-         if Commit.bot?(name) do
-           [
-             content_tag(:i, "", class: "fas fa-robot text-gray-dark"),
-             " ",
-             Commit.botless_username(name),
-           ]
-         else
-           name
-         end
-       end)
-    |> Enum.map(& content_tag(:b, &1))
-    |> Enum.intersperse(" and ")
-  end
 end
--- lib/remit_web/live_view_helpers.ex
+++ lib/remit_web/live_view_helpers.ex
@@ -1,41 +1,50 @@
 defmodule RemitWeb.LiveViewHelpers do
   alias Remit.Commit
+  import Phoenix.HTML.Tag
+
+  @small_commit_px 25
+
+  def github_avatar_sized_spacer(:small_commit) do
+    content_tag(:div, "", style: "width: #{@small_commit_px}px")
+  end
 
   def github_avatar(_, _, opts \\ [])
 
   def github_avatar(nil, _, _), do: nil
   def github_avatar(username, :comment, opts), do: github_avatar(username, 20, opts)
-  def github_avatar(username, :small_commit, opts), do: github_avatar(username, 25, opts)
+  def github_avatar(username, :small_commit, opts), do: github_avatar(username, @small_commit_px, opts)
 
   def github_avatar(username, size, opts) do
-    extra_classes = Keyword.get(opts, :class, "")
-    alt = Keyword.get(opts, :alt, username)
-
     username = Commit.botless_username(username)
 
-    # Setting CSS height because the `size` param is not always respected.
-    # Not setting CSS width because if the image is missing and we show the alt text, it should use more width.
-    # line-height so the alt text lines up.
-    Phoenix.HTML.Tag.img_tag(
-      "https://github.com/#{username}.png?size=#{size}",
-      alt: alt,
-      title: username,
-      class: "rounded-sm font-bold truncate #{extra_classes}",
-      style: "height: #{size}px; min-width: #{size}px; line-height: #{size}px"
-    )
+    extra_classes = Keyword.get(opts, :class, "")
+    tooltip = Keyword.get(opts, :tooltip, username)
+    tooltip_pos = Keyword.get(opts, :tooltip_pos, "up")
+
+    # Setting CSS dimensions because the `size` param is not always respected.
+    content_tag(:span, tooltip_attributes(tooltip, pos: tooltip_pos) ++ [class: "block #{extra_classes}"]) do
+      img_tag(
+        "https://github.com/#{username}.png?size=#{size}",
+        alt: "",
+        class: "rounded-sm bg-gray-mid",
+        style: "height: #{size}px; width: #{size}px;"
+      )
+    end
   end
 
-  def tooltip_attributes(text) do
+  def tooltip_attributes(nil), do: []
+  def tooltip_attributes(text, opts \\ []) do
     # http://kazzkiq.github.io/balloon.css/
+    pos = Keyword.get(opts, :pos, "up")
     [
-      "data-balloon-pos": "up",  # Above.
+      "data-balloon-pos": pos,
       "data-balloon-blunt": "",  # No animation delay.
       "aria-label": text,
     ]
   end
 
-  def render_tooltip(text) do
-    tooltip_attributes(text)
+  def render_tooltip(text, opts \\ []) do
+    tooltip_attributes(text, opts)
     |> Enum.map(fn {k, v} -> [
       Atom.to_string(k),
       {:safe, ~s(=")},
