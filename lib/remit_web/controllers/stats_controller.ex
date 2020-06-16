3469c7bbed8eac4940fef1f97677426db5200c02
Henrik Nyh <henrik@nyh.se>
2020-06-17 00:27:46 +0100

    More stats

--- lib/remit_web/controllers/stats_controller.ex
+++ lib/remit_web/controllers/stats_controller.ex
@@ -9,9 +9,29 @@ defmodule RemitWeb.StatsController do
       |> where([c], is_nil(c.reviewed_at))
       |> select([c], %{
         "unreviewed_count" => count(c.id),
-        "oldest_unreviewed_in_seconds" => fragment("ROUND(EXTRACT(EPOCH FROM (TIMEZONE('utc', NOW()) - MIN(inserted_at))))"),
+        "oldest_unreviewed_in_seconds" => fragment("ROUND(EXTRACT(EPOCH FROM (TIMEZONE('utc', NOW()) - MIN(inserted_at)))::numeric)::integer"),
       })
       |> Repo.one()
+
+    per_reviewer_counts =
+      Commit
+      |> where([c], c.updated_at > ago(10, "day"))
+      |> group_by([c], fragment("LOWER(reviewed_by_username)"))
+      |> select([c], {fragment("LOWER(reviewed_by_username)"), count(c.id)})
+      |> Repo.all()
+
+    recent_commits_count = per_reviewer_counts |> Enum.map(&elem(&1, 1)) |> Enum.sum()
+
+    recent_reviews =
+      per_reviewer_counts
+      |> Enum.filter(&elem(&1, 0))
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.sort()
+
+    data = Map.merge(data, %{
+      "recent_commits_count" => recent_commits_count,
+      "recent_reviews" => recent_reviews,
+    })
 
     json(conn, data)
   end
--- test/remit_web/controllers/api/stats_controller_test.exs
+++ test/remit_web/controllers/api/stats_controller_test.exs
@@ -4,15 +4,27 @@ defmodule RemitWeb.StatsControllerTest do
 
   test "it gives some stats" do
     now = DateTime.utc_now()
+
+    # Too old to count in reviewer stats.
+    secs_for_11_days = 60 * 60 * 24 * 11
+    Factory.insert!(:commit, reviewed_at: now, updated_at: DateTime.add(now, -secs_for_11_days), reviewed_by_username: "baz")
+
     Factory.insert!(:commit, reviewed_at: nil, inserted_at: DateTime.add(now, -100))
     Factory.insert!(:commit, reviewed_at: nil, inserted_at: DateTime.add(now, -50))
-    Factory.insert!(:commit, reviewed_at: DateTime.utc_now())
+    Factory.insert!(:commit, reviewed_at: now, reviewed_by_username: "foo")
+    Factory.insert!(:commit, reviewed_at: now, reviewed_by_username: "FOO")
+    Factory.insert!(:commit, reviewed_at: now, reviewed_by_username: "bar")
 
     conn = get_stats()
 
     assert json_response(conn, 200) == %{
       "unreviewed_count" => 2,
       "oldest_unreviewed_in_seconds" => 100,
+      "recent_commits_count" => 5,
+      "recent_reviews" => [
+        ["bar", 1],
+        ["foo", 2],  # Normalised to lowercase.
+      ],
     }
   end
 
@@ -22,6 +34,8 @@ defmodule RemitWeb.StatsControllerTest do
     assert json_response(conn, 200) == %{
       "unreviewed_count" => 0,
       "oldest_unreviewed_in_seconds" => nil,
+      "recent_commits_count" => 0,
+      "recent_reviews" => [],
     }
   end
 
