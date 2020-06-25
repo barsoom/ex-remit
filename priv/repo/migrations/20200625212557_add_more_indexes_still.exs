f2413f0ebc22c5894b5a445990748d027e237c3e
Henrik Nyh <henrik@nyh.se>
2020-06-25 22:29:55 +0100

    Oops, fix index name

--- priv/repo/migrations/20200625212557_add_more_indexes_still.exs
+++ priv/repo/migrations/20200625212557_add_more_indexes_still.exs
@@ -2,6 +2,6 @@ defmodule Remit.Repo.Migrations.AddMoreIndexesStill do
   use Ecto.Migration
 
   def change do
-    create index(:comments, ["lower(commenter_username)"], name: :comments_lower_username_index)
+    create index(:comments, ["lower(commenter_username)"], name: :comments_lower_commenter_username_index)
   end
 end
