a2250e9966d4c99744d601c8f9db84757e6da361
Henrik Nyh <henrik@nyh.se>
2020-07-03 11:15:05 +0100

    Add anchorbuster to fix issue navigating to not-loaded comments

--- lib/remit_web/live/comment_component.ex
+++ lib/remit_web/live/comment_component.ex
@@ -3,10 +3,12 @@ defmodule RemitWeb.CommentComponent do
   alias Remit.{Commit, Comment, Utils}
 
   # We get a comment URL via webhook, but the anchor is incorrect for line comments. This has been reported.
+  #
+  # If comment 1 on commit A is open on GitHub and we click the link for comment 2 on commit A, Fluid.app will try to navigate to that anchor without reloading the page. But comment 2 may not have been present when the commit A page was loaded, so nothing happens. We add a `remit_anchorbuster` parameter (that GitHub ignores) to force navigation.
   defp github_url(%Comment{path: nil, github_id: id, commit: %Commit{sha: sha, owner: o, repo: r}}) do
-    "https://github.com/#{o}/#{r}/commit/#{sha}#commitcomment-#{id}"
+    "https://github.com/#{o}/#{r}/commit/#{sha}?remit_anchorbuster=#{id}#commitcomment-#{id}"
   end
   defp github_url(%Comment{github_id: id, commit: %Commit{sha: sha, owner: o, repo: r}}) do
-    "https://github.com/#{o}/#{r}/commit/#{sha}#r#{id}"
+    "https://github.com/#{o}/#{r}/commit/#{sha}?remit_anchorbuster=#{id}#r#{id}"
   end
 end
