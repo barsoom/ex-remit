b5c72316b77559f9e4454b9949008179e86052f9
Henrik Nyh <henrik@nyh.se>
2021-09-28 12:53:59 +0100

    Whitespace

--- lib/remit_web/live/no_username_component.ex
+++ lib/remit_web/live/no_username_component.ex
@@ -7,7 +7,7 @@ defmodule RemitWeb.NoUsernameComponent do
     ~H"""
       <span
         class="bg-gray-light rounded py-px px-1 text-gray-dark"
-        {if tooltip?, do: tooltip_attributes(~s(Read about usernames under the "Settings" tab.), pos: "up-left"), else: [] }
+        {if tooltip?, do: tooltip_attributes(~s(Read about usernames under the "Settings" tab.), pos: "up-left"), else: []}
       ><i class="fas fa-exclamation-circle text-red-600"></i> no username</span>
     """
   end
