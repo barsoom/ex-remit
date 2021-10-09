aa1a3d367a8f215bb5bd6e94e1a677efee6908c8
Henrik Nyh <henrik@nyh.se>
2021-10-09 19:57:16 +0100

    Doc fixed line comment anchors
    Per GitHub support over email to my personal email, subject '[GitHub Support] - Bug: commit_comment webhook can have wrong anchor'.

--- lib/remit_web/live/comment_component.ex
+++ lib/remit_web/live/comment_component.ex
@@ -2,7 +2,8 @@ defmodule RemitWeb.CommentComponent do
   use RemitWeb, :live_component
   alias Remit.{Commit, Comment, Utils}
 
-  # We get a comment URL via webhook, but the anchor is incorrect for line comments. This has been reported.
+  # We get a comment URL via webhook, but the anchor used to be incorrect for line comments.
+  # It's supposed to be fixed per 2021-10-04; if we like, we could look into using it.
   #
   # If comment 1 on commit A is open on GitHub and we click the link for comment 2 on commit A, Fluid.app will try to navigate to that anchor without reloading the page. But comment 2 may not have been present when the commit A page was loaded, so nothing happens. We add a `remit_anchorbuster` parameter (that GitHub ignores) to force navigation.
   defp github_url(%Comment{path: nil, github_id: id, commit: %Commit{sha: sha, owner: o, repo: r}}) do
