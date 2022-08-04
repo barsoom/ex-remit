b3ab520cc2dd59a03cf9718eb4059842defa8d61
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-08-04 14:23:57 +0200

    replace the deprecated sigil_E with sigil_H

--- lib/remit_web/views/layout_view.ex
+++ lib/remit_web/views/layout_view.ex
@@ -1,12 +1,10 @@
 defmodule RemitWeb.LayoutView do
   use RemitWeb, :view
 
-  defp tab(action, text, icon, socket, live_action, params \\ []) do
-    params = if Keyword.values(params) |> Enum.all?(&(!&1)), do: [], else: params
-
-    live_patch(to: Routes.tabs_path(socket, action, params), class: "tabs__tab tabs__tab--#{action} #{if live_action == action, do: "tabs__tab--current"}") do
-      ~E"""
-      <i class="fas <%= icon %>"></i>
+  defp tab(assigns, action, text, icon, socket, live_action) do
+    live_patch(to: Routes.tabs_path(socket, action), class: "tabs__tab tabs__tab--#{action} #{if live_action == action, do: "tabs__tab--current"}") do
+      ~H"""
+      <i class={["fas", icon]}></i>
       <span class="tabs__tab__text"><%= text %></span>
       """
     end
