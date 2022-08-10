9ef88226ab6a2d0f1c581a08111d160e442a03b4
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-08-10 10:18:04 +0200

    revert the comment resolution changes

--- lib/remit/comments.ex
+++ lib/remit/comments.ex
@@ -1,6 +1,5 @@
 defmodule Remit.Comments do
   alias Remit.{Repo, Comment, CommentNotification}
-  alias Ecto.Multi
   import Ecto.Query
 
   def list_notifications(opts) when is_list(opts) do
@@ -14,56 +13,25 @@ defmodule Remit.Comments do
   def resolve(id) do
     now = DateTime.utc_now()
 
-    get_notification = from n in CommentNotification, where: n.id == ^id, preload: [comment: :commit]
-
-    resolve_notification = fn _, %{notification: notification} ->
-      notification
+    comment =
+      Repo.get_by(CommentNotification, id: id)
       |> Ecto.Changeset.change(resolved_at: now)
-      |> Repo.update()
-    end
-
-    resolve_comment = fn _, %{notification: notification} ->
-      commit = notification.comment.commit
-      authors = if commit, do: commit.usernames, else: []
-
-      notification.comment
-      |> Comment.resolve_changeset(notification.username, now, authors)
-      |> Repo.update()
-    end
-
-    # The rule is that only the first resolver will get to also resolve the
-    # comment. A higher isolation level than READ COMMITTED is required to
-    # consistently enforce this without data races.
-    #
-    # The unlucky transaction will error out; Postgres transaction handling
-    # guidelines recommend having a retry mechanism for such cases. This can be
-    # added later though, since this is unlikely to be a frequent problem in
-    # practice, and its only impact is UX.
-    {:ok, result} = Multi.new()
-                    |> Multi.run(:isolation_level, fn _, _ -> Repo.query("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ") end)
-                    |> Multi.one(:notification, get_notification)
-                    |> Multi.run(:updated_notification, resolve_notification)
-                    |> Multi.run(:comment, resolve_comment)
-                    |> Repo.transaction()
+      |> Repo.update!()
 
     broadcast_change()
 
-    result.updated_notification
+    comment
   end
 
   def unresolve(id) do
-    notification = Repo.get_by(CommentNotification, id: id)
-                   |> Repo.preload(:comment)
-    comment = notification.comment
-
-    {:ok, result} = Multi.new()
-                    |> Multi.update(:notification, Ecto.Changeset.change(notification, resolved_at: nil))
-                    |> Multi.update(:comment, Comment.unresolve_changeset(comment, notification.username))
-                    |> Repo.transaction()
+    comment =
+      Repo.get_by(CommentNotification, id: id)
+      |> Ecto.Changeset.change(resolved_at: nil)
+      |> Repo.update!()
 
     broadcast_change()
 
-    result.notification
+    comment
   end
 
   def subscribe, do: Phoenix.PubSub.subscribe(Remit.PubSub, "comments")
@@ -83,8 +51,8 @@ defmodule Remit.Comments do
 
     query =
       case resolved_filter do
-        "unresolved" -> from [n, c] in query, where: is_nil(n.resolved_at) and is_nil(c.resolved_at), order_by: [desc: :id]
-        "resolved" -> from [n, c] in query, where: not (is_nil(n.resolved_at) and is_nil(c.resolved_at)), order_by: [desc: fragment("coalesce(?, ?)", field(n, :resolved_at), field(c, :resolved_at))]
+        "unresolved" -> from n in query, where: is_nil(n.resolved_at), order_by: [desc: :id]
+        "resolved" -> from n in query, where: not is_nil(n.resolved_at), order_by: [desc: :resolved_at]
         "all" -> from query, order_by: [desc: :id]
       end
 
