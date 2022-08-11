ce1ae81099cc6c5a0d29e4a9235116d2495a0672
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-08-11 13:36:03 +0200

    lint: disable Design.AliasUsage
    This nudges you to create aliases for deeply nested modules, and it is
    routinely violated even by default Phoenix boilerplate. Doesn't seem to
    be worth it at the moment.

--- .credo.exs
+++ .credo.exs
@@ -82,8 +82,6 @@
           # You can customize the priority of any check
           # Priority values are: `low, normal, high, higher`
           #
-          {Credo.Check.Design.AliasUsage,
-           [priority: :low, if_nested_deeper_than: 2, if_called_more_often_than: 0]},
           # You can also customize the exit_status of each check.
           # If you don't want TODO comments to cause `mix credo` to fail, just
           # set this value to 0 (zero).
@@ -196,12 +194,14 @@
           {Credo.Check.Warning.LeakyEnvironment, []},
           {Credo.Check.Warning.MapGetUnsafePass, []},
           {Credo.Check.Warning.MixEnv, []},
-          {Credo.Check.Warning.UnsafeToAtom, []}
+          {Credo.Check.Warning.UnsafeToAtom, []},
 
           # {Credo.Check.Refactor.MapInto, []},
 
-          #
+          {Credo.Check.Design.AliasUsage,
+          [priority: :low, if_nested_deeper_than: 2, if_called_more_often_than: 0]},
           {Credo.Check.Readability.AliasOrder, []},
+          #
           # Custom checks can be created using `mix credo.gen.check`.
           #
         ]
