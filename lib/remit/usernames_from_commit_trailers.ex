d3d577bb5ee03ec2ee83ba44ed03ca5d242dcd01
Henrik Nyh <henrik@nyh.se>
2022-09-16 19:44:23 +0200

    Refactor UsernamesFromCommitTrailers
    E.g. some regex cleanup.

--- lib/remit/usernames_from_commit_trailers.ex
+++ lib/remit/usernames_from_commit_trailers.ex
@@ -1,28 +1,28 @@
 defmodule Remit.UsernamesFromCommitTrailers do
   @moduledoc false
-  @trailer_email_re ~r/<(.*)>/ix
-  @user_from_email_re ~r/\d*?\+?([^\+]*)@users\.noreply\.github\.com/ix
+  @co_author_line_re ~r/^Co-authored-by: .*<(.+)>/im
+  @github_email_re ~r/([^+]+)@users\.noreply\.github\.com\z/i
 
   def call(message) when is_binary(message) do
     message
-    |> String.split("\n")
-    |> Enum.filter(fn line -> String.starts_with?(line, "Co-authored-by") end)
-    |> Enum.flat_map(fn trailer -> Regex.scan(@trailer_email_re, trailer, capture: :all_but_first) end)
-    |> List.flatten()
-    |> Enum.uniq()
+    |> find_co_author_emails
     |> Enum.flat_map(&usernames_from_email/1)
-    |> Enum.reject(&is_nil/1)
+    |> Enum.uniq()
   end
 
   def call(_message), do: []
 
-  defp usernames_from_email(email) when is_binary(email) do
-    if String.contains?(email, "users.noreply.github.com") do
-      Regex.scan(@user_from_email_re, email, capture: :all_but_first)
-      |> List.flatten()
-    else
-      # TODO figure out a way to get the github username from an email
-      []
+  defp find_co_author_emails(message) do
+    Regex.scan(@co_author_line_re, message, capture: :all_but_first)
+    |> List.flatten()
+  end
+
+  defp usernames_from_email(email) do
+    case Regex.run(@github_email_re, email, capture: :all_but_first) do
+      [username] -> [username]
+      nil ->
+         # TODO figure out a way to get the github username from an email
+        []
     end
   end
 end
