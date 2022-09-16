23c8eb0b6dc92a067cd787e2d868d9820b35dcda
Henrik Nyh <henrik@nyh.se>
2022-09-16 19:53:38 +0200

    UsernamesFromCommitTrailersTest cleanup
    Inline messages so it's clearer what we test.
    
    Don't rely on vaguely named factory for the 'no co-authors' test.

--- test/remit/usernames_from_commit_trailers_test.exs
+++ test/remit/usernames_from_commit_trailers_test.exs
@@ -4,7 +4,12 @@ defmodule Remit.UsernamesFromCommitTrailersTest do
 
   test "it parses the email address from the commit trailer" do
     result =
-      Faker.message_with_co_authors("My fancy commit", ["dwight <dwight.shrute@users.noreply.github.com>", "michael <123+michael.scott@users.noreply.github.com>"])
+      """
+      My fancy commit
+
+      Co-authored-by: dwight <dwight.shrute@users.noreply.github.com>
+      Co-authored-by: michael <123+michael.scott@users.noreply.github.com>
+      """
       |> UsernamesFromCommitTrailers.call()
 
     assert result == ["dwight.shrute", "michael.scott"]
@@ -12,19 +17,32 @@ defmodule Remit.UsernamesFromCommitTrailersTest do
 
   test "it only includes co-authors using github noreply emails" do
     result =
-      Faker.message_with_co_authors("My fancy commit", ["dwight shrute <dwight.shrute@users.noreply.github.com>", "ryan howard <ryan.howard@wuphf.com>"])
+      """
+      My fancy commit
+
+      Co-authored-by: dwight <dwight.shrute@users.noreply.github.com>
+      Co-authored-by: ryan howard <ryan.howard@wuphf.com>
+      """
       |> UsernamesFromCommitTrailers.call()
 
     assert result == ["dwight.shrute"]
   end
 
   test "it returns an empty list if there are no co-authors" do
-    result = UsernamesFromCommitTrailers.call(Faker.message())
+    result =
+      """
+      My fancy commit
+
+      My description.
+      """
+      |> UsernamesFromCommitTrailers.call()
+
     assert result == []
   end
 
   test "it returns an empty list if there is no commit message" do
     result = UsernamesFromCommitTrailers.call(nil)
+
     assert result == []
   end
 end
