0be238a66143827492c1562b77f62a7381064489
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-12-16 10:39:35 +0100

    chore: update webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
@@ -11,8 +11,8 @@ module.exports = (env, options) => {
   return {
     optimization: {
       minimizer: [
-        new TerserPlugin({cache: true, parallel: true, sourceMap: devMode}),
-        new OptimizeCSSAssetsPlugin({})
+        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
+        new CssMinimizerPlugin()
       ]
     },
     entry: {
@@ -44,8 +44,8 @@ module.exports = (env, options) => {
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({filename: '../css/app.css'}),
-      new CopyWebpackPlugin({patterns: [{from: 'static/', to: '../'}]})
+      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+      new CopyWebpackPlugin({ patterns: [{ from: 'static/', to: '../' }] })
     ]
   }
 };
