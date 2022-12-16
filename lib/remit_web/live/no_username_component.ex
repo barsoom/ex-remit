9a9bd37ab37a8a89aba1c0c0c1e05917b04ac7c5
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-12-16 12:11:25 +0100

    fix heex warning

--- lib/remit_web/live/no_username_component.ex
+++ lib/remit_web/live/no_username_component.ex
@@ -3,12 +3,10 @@ defmodule RemitWeb.NoUsernameComponent do
   use RemitWeb, :live_component
 
   def render(assigns) do
-    tooltip? = Map.get(assigns, :tooltip?, true)
-
     ~H"""
       <span
         class="bg-gray-light rounded py-px px-1 text-gray-dark"
-        {if tooltip?, do: tooltip_attributes(~s(Read about usernames under the "Settings" tab.), pos: "up-left"), else: []}
+        {if @tooltip?, do: tooltip_attributes(~s(Read about usernames under the "Settings" tab.), pos: "up-left"), else: []}
       ><i class="fas fa-exclamation-circle text-red-600"></i> no username</span>
     """
   end
