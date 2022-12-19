ff476b7da874dab55de7ad254a16fdacef1c0994
Valeri Sokolov <valeri.sokolov@auctionet.com>
2022-12-19 16:08:28 +0100

    sort after grouping
    Putting things into a map destroys previous sorting.

--- lib/remit/project.ex
+++ lib/remit/project.ex
@@ -8,7 +8,8 @@ defmodule Remit.Project do
     get_all_query()
     |> Repo.all()
     |> Enum.group_by(fn {commit, _} -> commit.repo end, fn {_, team} -> team end)
-    |> Enum.map(fn {project, teams} = element -> if teams == [nil], do: {project, []}, else: element end)
+    |> Enum.map(fn {project, teams} -> if teams == [nil], do: {project, []}, else: {project, Enum.sort_by(teams, & &1.name)} end)
+    |> Enum.sort_by(&elem(&1, 0))
   end
 
   defp project_names_query, do: from c in Commit, select: [:repo], distinct: true
@@ -16,7 +17,6 @@ defmodule Remit.Project do
   defp get_all_query do
     from commit in subquery(project_names_query()),
       left_join: team in Team, on: commit.repo in team.projects,
-      select: {commit, team},
-      order_by: {:asc, commit.repo}
+      select: {commit, team}
   end
 end
