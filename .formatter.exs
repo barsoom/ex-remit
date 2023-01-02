0cdb15047066b78ff694b6e830c66cc1f3a26e7d
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2023-01-02 15:34:04 +0100

    chore: make mix format heex files

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 [
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["*.{ex,exs,heex}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
   subdirectories: ["priv/*/migrations"],
   line_length: 999
 ]
