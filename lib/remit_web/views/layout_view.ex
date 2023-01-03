a326afdd50efe40705af58bcd9fd10b8c126de3d
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2023-01-03 14:05:59 +0100

    feat(ui): allow tabs to display a notification icon

--- lib/remit_web/views/layout_view.ex
+++ lib/remit_web/views/layout_view.ex
@@ -7,15 +7,18 @@ defmodule RemitWeb.LayoutView do
     <.link patch={Routes.tabs_path(Endpoint, @action)} class={tab_class(assigns)}>
       <i class={["fas", @icon]}></i>
       <span class="tabs__tab__text"><%= @text %></span>
+      <%= if @show_notification_bell do %>
+        <span class="inline-block absolute top-2 right-2 w-2 h-2 bg-red-600 border rounded-full"></span>
+      <% end %>
     </.link>
     """
   end
 
   defp tab_class(%{action: action, current_action: action}) do
-    ["tabs__tab", "tabs__tab--#{action}", "tabs__tab--current"]
+    ["tabs__tab", "tabs__tab--#{action}", "relative", "tabs__tab--current"]
   end
 
   defp tab_class(%{action: action}) do
-    ["tabs__tab", "tabs__tab--#{action}"]
+    ["tabs__tab", "tabs__tab--#{action}", "relative"]
   end
 end
