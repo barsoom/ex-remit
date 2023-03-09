6240f9e210f932f74b36eb3e6f16818ebc980666
Tomas Skogberg <tomas.skogberg@gmail.com>
2023-03-09 08:10:39 +0100

    chore: update Hex dependencies
    Phoenix 1.7 comes with some interesting features, like verified routes
    which we could implement.
    
    https://phoenixframework.org/blog/phoenix-1.7-final-released
    
    Diffs:
    
    [NEW!] https://hex.pm/packages/websock
    [NEW!] https://hex.pm/packages/websock_adapter
    https://diff.hex.pm/diff/castore/1.0.0..1.0.1
    https://diff.hex.pm/diff/floki/0.34.1..0.34.2
    https://diff.hex.pm/diff/phoenix/1.6.16..1.7.1
    https://diff.hex.pm/diff/phoenix_html/3.3.0..3.3.1
    https://diff.hex.pm/diff/phoenix_live_view/0.18.15..0.18.16
    
    Generated-by: dev update

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule Remit.MixProject do
       {:phoenix_live_view, ">= 0.16.4"},
       {:floki, "> 0.0.0", only: :test},
       {:phoenix_html, "> 0.0.0"},
+      {:phoenix_view, "~> 2.0.0"},
       {:phoenix_live_reload, "> 0.0.0", only: :dev},
       {:phoenix_live_dashboard, "> 0.0.0"},
       {:telemetry_metrics, "> 0.0.0"},
